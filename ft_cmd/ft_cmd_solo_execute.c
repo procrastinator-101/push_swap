5b341e34dc24bd5c047509468d06b2e6db8787f5
Younes Arroubi <yarroubi@e1r4p4.1337.ma>
2021-07-17 12:15:17 +0100

    atomic cases updated

--- ft_cmd/ft_cmd_solo_execute.c
+++ ft_cmd/ft_cmd_solo_execute.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_container_pop.c                                 :+:      :+:    :+:   */
+/*   ft_cmd_solo_execute.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yarroubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/17 10:18:23 by yarroubi          #+#    #+#             */
-/*   Updated: 2021/07/17 11:56:50 by yarroubi         ###   ########.fr       */
+/*   Created: 2021/07/17 12:07:20 by yarroubi          #+#    #+#             */
+/*   Updated: 2021/07/17 12:10:04 by yarroubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_container.h"
+#include "ft_cmd.h"
 
-int	ft_container_pop(t_container *container)
+int	ft_cmd_solo_execute(t_container *container, t_cmd cmd)
 {
-	int		ret;
-
-	ret = ft_stack_pop(container->stack);
-	if (container->stack->end <= container->chunks->previous->first)
-		ft_pair_removeback(&(container->chunks));
-	return (ret);
+	if (cmd.type == SWAP)
+		ft_stack_swap(container->stack);
+	else if (cmd.type == ROTATE)
+		ft_stack_rotate(container->stack);
+	else if (cmd.type == REVROTATE)
+		ft_stack_reverse_rotate(container->stack);
+	return (0);
 }
